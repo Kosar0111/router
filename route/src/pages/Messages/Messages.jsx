efb95aff2f14366ef6944989b40898eab29e216a
Konstantin Loktiev <kosar0111@gmail.com>
2022-08-25 17:42:32 +0300

    fill in pages

--- route/src/pages/Messages/Messages.jsx
+++ route/src/pages/Messages/Messages.jsx
@@ -1,7 +1,7 @@
 import './Messages.css'
 
 const Messages = () => {
-  return <div className='messages'>This's block information about your the list of messages </div>
+  return <div className='messages'>This's block information about your list of messages </div>
 }
 
 export default Messages
